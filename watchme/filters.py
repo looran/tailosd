240178df82c73d294bd9d444f052118923046d2c
Laurent Ghigonis <laurent@gouloum.fr>
2014-07-13 01:20:18 +0200

    fix iptables log parse, add debug mode

--- watchme/filters.py
+++ watchme/filters.py
@@ -44,43 +44,50 @@ class Watchme_filter_default_syslog(object):
         return evt
 
 class Watchme_filter_default_syslog_iptables(object):
-    def __init__(self):
-        pass
+    def __init__(self, debug=False):
+        self.debug = debug
 
     def filter(self, evt):
         if "iptables-drop:" in evt.content:
+            if self.debug: print "Watchme_filter_default_syslog_iptables: action drop"
             action="drop"
         elif "iptables-accept:" in evt.content:
+            if self.debug: print "Watchme_filter_default_syslog_iptables: action accept"
             action="accept"
         else:
             return evt # not for us
 
         # Content rewording
-        m_ip = re.match(r".* iptables-(?P<action>[a-z]*): IN=(?P<iface_in>\w*) OUT=(?P<iface_out>\w*) MAC=(?P<mac>[0-9a-f:]*) SRC=(?P<ip_src>[0-9\.]*) DST=(?P<ip_dst>[0-9\.]*) LEN=(?P<len>[0-9]*) TOS=(?P<tos>[0-9a-zx]*) PREC=(?P<prec>[0-9a-zx]*) TTL=(?P<ttl>[0-9]*) ID=(?P<id>[0-9]*) .* PROTO=(?P<proto>[0-9A-Z]*)", evt.content)
+        m_ip = re.match(r".* iptables-(?P<action>[a-z]*): IN=(?P<iface_in>\w*) OUT=(?P<iface_out>\w*) MAC=(?P<mac>[0-9a-f:]*) SRC=(?P<ip_src>[0-9\.]*) DST=(?P<ip_dst>[0-9\.]*) LEN=(?P<len>[0-9]*) TOS=(?P<tos>[0-9a-zx]*) PREC=(?P<prec>[0-9a-zA-Zx]*) TTL=(?P<ttl>[0-9]*) ID=(?P<id>[0-9]*).* PROTO=(?P<proto>[0-9A-Z]*)", evt.content)
         if m_ip:
+            if self.debug: print "Watchme_filter_default_syslog_iptables: m_ip"
             d_ip = m_ip.groupdict()
             mac_src = d_ip['mac'][18:35]
             mac_dst = d_ip['mac'][:17]
             d_ip['mac'] = "%s > %s" % (mac_src, mac_dst)
             if d_ip['proto'] == "TCP":
+                if self.debug: print "Watchme_filter_default_syslog_iptables: TCP"
                 m_tcp = re.match(r".* SPT=(?P<port_src>[0-9]*) DPT=(?P<port_dst>[0-9]*) WINDOW=(?P<win>[0-9]*) RES=(?P<res>[0-9a-zx]*) (?P<flags>.*) URGP=(?P<urgp>[0-9]*)", evt.content)
                 if m_tcp:
                     d = m_tcp.groupdict()
                     evt.content = "%s: %s %s %s:%s > %s:%s %s [%s] %s" % (
                         action, d_ip['iface_in'], d_ip['mac'], d_ip['ip_src'], d['port_src'], d_ip['ip_dst'], d['port_dst'], d['flags'], d_ip['len'], d_ip['ttl'])
             elif d_ip['proto'] == "UDP":
+                if self.debug: print "Watchme_filter_default_syslog_iptables: UDP"
                 m_udp = re.match(r".* SPT=(?P<port_src>[0-9]*) DPT=(?P<port_dst>[0-9]*) LEN=(?P<len>[0-9]*)", evt.content)
                 if m_udp:
                     d = m_udp.groupdict()
                     evt.content = "%s: %s %s %s:%s > %s:%s UDP [%s] %s" % (
                         action, d_ip['iface_in'], d_ip['mac'], d_ip['ip_src'], d['port_src'], d_ip['ip_dst'], d['port_dst'], d_ip['len'], d_ip['ttl'])
             elif d_ip['proto'] == "ICMP":
+                if self.debug: print "Watchme_filter_default_syslog_iptables: ICMP"
                 m_icmp = re.match(r".* TYPE=(?P<type>[0-9]*) CODE=(?P<code>[0-9]*) ID=(?P<id>[0-9]*) SEQ=(?P<seq>[0-9]*)", evt.content)
                 if m_icmp:
                     d = m_icmp.groupdict()
                     evt.content = "%s: %s %s %s > %s ICMP %s-%s [%s] %s" % (
                         action, d_ip['iface_in'], d_ip['mac'], d_ip['ip_src'], d_ip['ip_dst'], d['type'], d['code'], d_ip['len'], d_ip['ttl'])
             else:
+                if self.debug: print "Watchme_filter_default_syslog_iptables: Unknown proto"
                 evt.content = "%s: %s %s %s > %s %s [%s] %s" % (
                     action, d_ip['iface_in'], d_ip['mac'], d_ip['ip_src'], d_ip['ip_dst'], d_ip['proto'], d_ip['len'], d_ip['ttl'])
 
